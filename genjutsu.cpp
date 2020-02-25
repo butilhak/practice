cb475d0a85619411f8024fc76ab716f3ee2ac72c
Dranem Mendoza <dranemrjmendoza@gmail.com>
2020-02-25 09:55:30 +0800

    genjutsu problem

---
+++ genjutsu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+
+    char *s="GOODLUCK";
+    for(int i=strlen(s)-1; i>=0; i--) {
+        for (int j=0; j<=i; j++)
+        cout << s[j];
+        cout << endl;
+    }
+
+    return 0;
+}
